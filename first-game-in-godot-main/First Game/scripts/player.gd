37c516fa99b07ebdc4ce485954292db7ed2281ef
Stanlee1234 <lustanleyz@gmail.com>
2025-09-27 21:19:59 -0700

    Finally added the Corpses!

--- first-game-in-godot-main/First Game/scripts/player.gd
+++ first-game-in-godot-main/First Game/scripts/player.gd
@@ -6,6 +6,9 @@ const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var corpse_counter = 0
+var is_sacrifice = false
+
 
 @onready var animated_sprite = $AnimatedSprite2D
 
@@ -28,13 +31,14 @@ func _physics_process(delta):
 		animated_sprite.flip_h = true
 	
 	# Play animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
+	if not is_sacrifice:
+		if is_on_floor() :
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
+			animated_sprite.play("jump")
 	
 	# Apply movement
 	if direction:
@@ -43,3 +47,20 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	process_sacrifice()
+
+func create_duplicate_sprite():
+	if not is_sacrifice:
+		return
+	var new_sprite = animated_sprite.duplicate()
+	is_sacrifice = false
+	corpse_counter += 1
+	add_sibling(new_sprite)
+	new_sprite.position = Vector2(self.position.x, self.position.y - 6)
+	new_sprite.name = "corpse" + str(corpse_counter)
+	
+func process_sacrifice():
+	if Input.is_action_just_pressed("sacrifice"):
+		animated_sprite.animation_finished.connect(create_duplicate_sprite)
+		is_sacrifice = true
+		animated_sprite.play("sacrifice")
