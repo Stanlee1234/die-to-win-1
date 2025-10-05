71f590684ed8caffaf7be0d70ec183787d29bb3b
Stanlee1234 <lustanleyz@gmail.com>
2025-10-05 11:33:51 -0700

    a few walkatime changes

--- first-game-in-godot-main/First Game/scripts/flag.gd
+++ first-game-in-godot-main/First Game/scripts/flag.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
 var score = 0
+var level = 1
 
 func _on_body_entered(body: Node2D) -> void:
 	print(body.name)
 	if body.name == "TileMap":
 		pass
 	if body.name == "Player":
-		var path = "res://Area/level_2.tscn"
+		level += 1
+		var path = "res://Area/level_" + str(level) + ".tscn"
 		get_tree().change_scene_to_file(path)
 		print("change level")
